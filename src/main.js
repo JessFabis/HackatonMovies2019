d189af438af668737bd960ed6f835a3a7581d234
gabycastro1403 <gaby09choco@gmail.com>
2019-02-27 15:44:45 -0600

    Subiendo primera interaccion con la data usando un input, local storage y fetch

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 //aqui van eventos DOM
+const user = document.getElementById("user");
+const search = document.getElementById("search");
+
+
+    
+
+search.addEventListener("click",()=>{
+    const valueUser = user.value;
+    console.log(valueUser)
+    const url = `http://www.omdbapi.com/?apikey=60fc1083&s=${valueUser}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+    localStorage.setItem("data",JSON.stringify(data))
+    })
+})
