9afe6ff8b1ffbfbeab2494f51952b79254c42190
J.Fabiola Juárez Z <jefajuza@hotmail.com>
2019-02-27 20:50:14 -0600

    en el main.js linea 11 se agrego una (s) para hacer el sitio seguro

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ let dataMovie = "";
 search.addEventListener("click",()=>{
     const valueUser = user.value;
     console.log(valueUser);
-    const url = `http://www.omdbapi.com/?apikey=60fc1083&s=${valueUser}`;
+    const url = `https://www.omdbapi.com/?apikey=60fc1083&s=${valueUser}`;
     fetch(url)
     .then(response => response.json())
     .then(data => {
